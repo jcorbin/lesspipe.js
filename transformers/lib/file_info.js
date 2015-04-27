db32fb686bc1ceab8cb158c6b213fe60307ebd13
Joshua T Corbin <jcorbin@wunjo.org>
2015-04-26 22:11:08 -0700

    Add type guessing based on extension

--- transformers/lib/file_info.js
+++ transformers/lib/file_info.js
@@ -1,8 +1,15 @@
 var fs = require('fs');
+var mime = require('mime');
 
 function FileInfo(name) {
     this.path = name || '';
     this.stat = maybeStat(this.path);
+    this.guessType = null;
+    this.guessCharset = null;
+    if (this.path) {
+        this.guessType = mime.lookup(this.path);
+        this.guessCharset = mime.charsets.lookup(this.type);
+    }
 }
 
 function maybeStat(path) {
